5f97cb9724b5fe133540e251ee24bc699e9ada14
exequielcabrera98 <execabrerasarrio@gmail.com>
2024-11-29 00:54:37 -0300

    categorías incompletas

--- Frontend/src/login/Inicio.jsx
+++ Frontend/src/login/Inicio.jsx
@@ -16,7 +16,7 @@ function Inicio() {
     const onSubmit = async (data) => {
         // Aquí irá la lógica de inicio de sesión
         try {
-            const response = await api.post("login_persona", data);
+            const response = await api.post("login", data);
             
             // Chequear si la estructura de la respuesta es la esperada
             if (response.status === 200 && response.data) {
