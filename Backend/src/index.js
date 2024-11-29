ff7ad85456f4d806b3f4d1db4be554c162d3261d
exequielcabrera98 <execabrerasarrio@gmail.com>
2024-11-28 21:52:44 -0300

    login

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import registerRoutes from "./routes/register.routes.js"
+import loginRoutes from "./routes/login.routes.js"
 
 
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 app.use(express.json());
 
 app.use("/api", registerRoutes)
+app.use("/api", loginRoutes)
 
 app.listen(3000)
 console.log("server o port: ",3000)
---
+++ Backend/src/routes/login.routes.js
@@ -0,0 +1,58 @@
+import { Router } from "express";
+import bcrypt from 'bcryptjs'
+import { PrismaClient } from "@prisma/client";
+import { createAccessToken } from "../libs/jwt.js";
+import dotenv from 'dotenv'
+
+const router = Router()
+const prisma = new PrismaClient()
+
+dotenv.config()
+
+router.post('/login', async (req, res) => {
+    try {
+      const { DNI, Contrasena } = req.body;
+  
+      // Validación básica de campos requeridos
+      if (!DNI || !Contrasena) {
+        return res.status(400).json({ error: 'Faltan campos obligatorios' });
+      }
+
+      const usuarioExistente = await prisma.persona.findUnique({
+        where: { DNI }
+      });
+
+      if (!usuarioExistente) {
+        return res.status(400).json({ error: 'Usuario no encontrado' });
+      }
+
+      const passwordMatch = await bcrypt.compare(Contrasena, usuarioExistente.Contrasena);
+
+      if(!passwordMatch) return res.status(400).json({message: "Correo o contraseña incorrectos."})
+
+      const token = await createAccessToken(usuarioExistente)
+      
+      res.cookie('token', token)
+
+      res.json({ 
+        DNI: usuarioExistente.DNI,
+        FechaNacimiento: usuarioExistente.FechaNacimiento,
+        Direccion: usuarioExistente.Direccion,
+        Apellido: usuarioExistente.Apellido,
+        Nombre: usuarioExistente.Nombre,
+        Rol: usuarioExistente.Rol,
+      });
+    } 
+    catch (error) {
+      console.error('Error al registrar usuario:', error);
+      
+      // Manejo de errores específicos de Prisma
+      if (error.code === 'P2002') {
+        return res.status(409).json({ error: 'Ya existe un usuario con este DNI' });
+      }
+  
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+
+export default router
